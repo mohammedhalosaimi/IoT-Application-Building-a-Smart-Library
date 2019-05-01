8a85003c5f122698be2ab6f59f2471da29964ffc
maodawei <shinemao@vip.qq.com>
2019-05-01 15:49:00 +1000

    db class 1.0 finished

--- rp/db.py
+++ rp/db.py
@@ -23,8 +23,21 @@ class database:
             
     @staticmethod
     def insertData(username,password,firstname,lastname,email):
-        pass
+        conn=database.connection()
+        with conn:
+            cur=conn.cursor()
+            try:
+                cur.execute("INSERT INTO ACCOUNT values(?,?)",(username,password))
+                cur.execute("INSERT INTO USER values(?,?,?)",(firstname,lastname,email,username))
+            except:
+                print("Username already exist!")
 
     @staticmethod
     def veriftData(username,password):
-        pass
+        conn=database.connection()
+        with conn:
+            cur=conn.cursor()
+            result=list(cur.execute("SELECT * FROM ACCOUNT WHERE USERNAME = ?",(username)))
+            if password != result[-1][-1] :
+                return True
+        return False
