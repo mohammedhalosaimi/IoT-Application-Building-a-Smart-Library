54f8def58278ae3cc22a33bf8bf3ae1d8ad07562
Bradels <Brad-Hill@hotmail.com.au>
2019-05-02 13:25:24 +1000

    Fixes

--- mp/menu.py
+++ mp/menu.py
@@ -16,6 +16,7 @@ class Menu:
             print("1. List Books")
             print("2. Insert Book")
             print("3. Quit")
+            print("4. Search book by title")
             selection = input("Select an option: ")
             print()
 
@@ -46,6 +47,14 @@ class Menu:
                 print("{} inserted successfully.".format(title))
             else:
                 print("{} failed to be inserted.".format(title))
+
+    def searchBookByTitle(self):
+        searchTerm = input("Enter your search term for the book: ")
+        print("--- Books ---")
+        print("{:<15} {}".format("Book ID", "Title"))
+        with cloud_db() as db:
+            for book in db.getBooksByTitle(searchTerm):
+                print("{:<15} {}".format(book[0], book[1]))
 
 if __name__ == "__main__":
     Menu().main()
