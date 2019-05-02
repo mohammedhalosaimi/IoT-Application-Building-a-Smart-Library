3d556270aa4ff77e3122f8f0a01253e70cfa92e6
Bradels <Brad-Hill@hotmail.com.au>
2019-05-02 10:35:39 +1000

    Testing initial connection with week 7 code.

--- mp/cloud_db.py
+++ mp/cloud_db.py
@@ -9,8 +9,55 @@ Copy the following code into it and adjust it to suit your DB.
 """
 import json
 import os
+import MySQLdb
 
 class cloud_db:
+    
+    #Largely based off week 7 code supplied.
+    DATABASE = "TestPeople"
+
+    def __init__(self, connection = None):
+        dbInfo = cloud_db.load_json()
+        if(connection == None):
+            connection = MySQLdb.connect(dbInfo["address"], dbInfo["username"],
+                dbInfo["password"], cloud_db.DATABASE)
+        self.connection = connection
+
+    def close(self):
+        self.connection.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
+
+    def createPersonTable(self):
+        with self.connection.cursor() as cursor:
+            cursor.execute("""
+                create table if not exists Person (
+                    PersonID int not null auto_increment,
+                    Name text not null,
+                    constraint PK_Person primary key (PersonID)
+                )""")
+        self.connection.commit()
+
+        def insertPerson(self, name):
+            with self.connection.cursor() as cursor:
+                cursor.execute("insert into Person (Name) values (%s)", (name,))
+            self.connection.commit()
+            return cursor.rowcount == 1
+
+        def getPeople(self):
+            with self.connection.cursor() as cursor:
+                cursor.execute("select PersonID, Name from Person")
+            return cursor.fetchall()
+        
+        def deletePerson(self, personID):
+            with self.connection.cursor() as cursor:
+                cursor.execute("delete from Person where PersonID = %s", (personID,))
+        self.connection.commit()
+
 
     @staticmethod
     def load_json():
@@ -20,5 +67,6 @@ class cloud_db:
             return json.load(j)
 
 if __name__ == "__main__":
-    values = cloud_db.load_json()
-    print(values["address"])
+    with cloud_db as db:
+            for person in db.getPeople():
+                print("{:<15} {}".format(person[0], person[1]))
