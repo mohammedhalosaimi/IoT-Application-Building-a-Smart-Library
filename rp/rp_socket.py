7dbb4467837c7ccd1dba0085e27315b1b789420a
Farid Farzin <farid.farzin@gmail.com>
2019-05-03 19:39:26 +1000

    Update rp_socket
    rp_socket is updated to be called from the login method

--- rp/rp_socket.py
+++ rp/rp_socket.py
@@ -1,18 +1,15 @@
-#!/usr/bin/env python3
 # Reference: https://realpython.com/python-sockets/
 # Documentation: https://docs.python.org/3/library/socket.html
 
 import socket
 import logging
 import json
-from user import user
 
 logging.basicConfig(filename="library.log", level = logging.ERROR)
 class rp_socket:
-    def main(self):
-        self.connection()
+
     # Call connection after successful login
-    def connection(self):        
+    def connection(self, user):    
         try:
             jsonData = self.readConfig()
             HOST = jsonData["hostname"]
@@ -20,26 +17,14 @@ class rp_socket:
             ADDRESS = (HOST, PORT)
             print(HOST)
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                print("Connecting to {}...".format(ADDRESS))
                 s.connect(ADDRESS)
-                print("Connected.")
-                # message = "loggedin" # to be removed
-                while True:
-                    # Call Login method, Method message should listen to MP for loggedout message
-                    # message = login().login() # replace with login method
-                    message = user.menu()
-
-                    if(message == "loggedin"):                
-                        s.sendall(message.encode())
-
+                while True:               
+                    s.sendall(user.encode())
                     # listen to message from server:
                     data = s.recv(4096)
-                    print("Received {} bytes of data decoded to: '{}'".format(
-                        len(data), data.decode()))
                     message = data.decode()
-                    # if(message == "loggedout"):
-                    #     loggedin = "loggedout"
-                    #     break
+                    if(message == "logout"):
+                        return message
         except  Exception as e:
             logging.error("RP Socket error: {}".format(str(e)))
             print(str(e))
@@ -52,5 +37,3 @@ class rp_socket:
             data = json.load(jsonFile)
         return data
 
-if __name__ == "__main__":
-    rp_socket().main()
